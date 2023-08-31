d3dd99160f917d1e1da4b88e858370ea0ea7e004
Isha Zaka <90307713+Ishazaka@users.noreply.github.com>
2023-08-31 18:55:24 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,19 @@
+// core data types
+// let y :number = 10;
+// let x: string = 'hello';
+// let isData: boolean = false;
+// console.warn(x, y, isData);
+// Number
+// number supports (floating, decimals, hexa-decimals, binary)
+var first = 10; //number
+var second = 0x67CF; // hexa-decimal
+var third = 301; //octal
+var fourth = 53; //binary
+console.log(first);
+console.log(second);
+console.log(third);
+console.log(fourth);
+// Array
+var names = ['johns', 'apple', 'mango'];
+// names.push(3)  //throw error
+names.push('watermelon');
